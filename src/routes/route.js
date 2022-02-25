9d2143d405f69464542218dd135d7913060b6f19
nav7033 <mandaln51@gmail.com>
2022-02-25 08:21:19 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,5 +4,62 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+let persons= [
+    {
+   
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40, 
+    votingsStaus:false
+ }
+ ];
+router.post("/election",function(request,response){
+    let votingAge=request.query.votingAge
+    let personArr=[];
+    for(let i=0;i<persons.length;i++){
+        if(persons[i].age>votingAge){
+            persons[i].votingStatus=true;
+            personArr.push(persons[i]);
+            
+        }
+           
+        
+        if(personArr.length>0){
+            response.send(personArr)
+        }
+        else{
+            response.send("pls enter valid  age ")
+        }
+       
+          
+}
+    
+    
+  
+})
+    
+
+     
+     
+
 
 module.exports = router;
