28707945b198145445f9fc1dd1e2dec15923cb0e
nav7033 <mandaln51@gmail.com>
2022-03-10 04:14:53 +0530

    task completed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,20 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken");
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
+    let userId=req.params.userId
+    let token = req.headers["x-auth-token"]
+    if(!token){
+        res.send({status:false,msg:"token required inside headers"})
+    }
+
+    let decodeToken=jwt.verify(token,'functionup-thorium')
+    if(!decodedToken){
+        res.send({status:false, msg:" token invalid"})
+    }
+    
+   
+
 
     next()
 }
@@ -8,5 +22,15 @@ const authenticate = function(req, req, next) {
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    let userId=req.params.userId
+    let token= req.headers["x-auth-token"]
+    let decodeToken=jwt.verify(token,"functionup-thorium")
+    let tokenUserId=decodeToken.userId
+    
+    if(tokenUserId !=userId){
+        res.send({status:false,msg:"this is not  valid person "})
+    }
     next()
 }
+module.exports.authenticate=authenticate
+module.exports.authorise=authorise
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
+    firstName: String, 
     lastName: String,
     mobile: {
         type: String,
